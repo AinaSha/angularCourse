feec409aba5ca6e8f4a7986e640a7d26af328968
Ainagul Shabdanova <shabdanova.ainagul.kk@gmail.com>
2024-04-25 17:49:02 +0600

    angular binding and create calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyCalculatorComponent } from './my-calculator/my-calculator.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCalculatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
