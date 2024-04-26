2b520a854e939f9bbaac11f51cedfa949e27d500
Ainagul Shabdanova <shabdanova.ainagul.kk@gmail.com>
2024-04-26 21:39:29 +0600

    feat: create my calcualtor module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyCalculatorComponent } from './my-calculator/my-calculator.component';
+import { MyCalculatorModule } from './calculator/my.calculator.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyCalculatorComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    MyCalculatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
